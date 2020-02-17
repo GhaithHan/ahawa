8f6c7b439fb6d558e8c95cbdc32f3d47e695fcbe
GhaithHan <hangh.evo@gmail.com>
2020-02-17 16:55:20 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var hahaha = function(){
+  if(navigator.userAgent.indexOf("chrome") == -1){
+    console.log("no browser chrome")
+  }else{
+    console.log("this is a chrome browser")
+  }
+}
+
+
+hahaha();
